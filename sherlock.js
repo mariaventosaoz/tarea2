e432f422590277f935755288eb69d3f277bb36cc
mariaventosaoz <mariaventosa2@gmail.com>
2015-08-26 13:26:13 -0500

    Update sherlock.js

--- sherlock.js
+++ sherlock.js
@@ -2,6 +2,21 @@
 
 _INPUT = "2\n3 9\n17 24";
 
+long = _INPUT.length
+veces = _INPUT.str(0,0);
+pos=1;
+
+for(k=0;k<veces;k++){
+  if (pos <= long){
+    a=_INPUT.str(pos,pos);
+    pos=pos+2;
+    b=_INPUT.str(pos,pos);
+    for(l=a;l<=b;l++){
+      
+    }
+  }
+}
+
 //Expected Output
 /*
 2
